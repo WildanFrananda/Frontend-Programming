3b437b6180960f323b3e51ed1bf33966c670a844
WildanFrananda <wildanrailfanskrl@gmail.com>
2023-03-17 11:34:14 +0700

    Task 4

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// Import semua component.
+import "./App.css"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+function App() {
+	return (
+		<div className="App">
+			<h2>This is Create React App</h2>
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// Membuat component Header.
+function Header() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
