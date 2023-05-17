e8a855ae9349c0d698df711da260c19804bc4296
WildanFrananda <wildanrailfanskrl@gmail.com>
2023-05-17 16:37:22 +0700

    Fix addMovieForm CSS

--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -74,7 +74,7 @@ function AddMovieForm(props) {
         <div className={styles.container}>
             <section className={styles.form}>
                     <div className={styles.form__left}>
-                        <img className={styles.image__form} src="https://picsum.photos/536/354" alt="" />
+                        <img className={styles.form__image} src="https://picsum.photos/536/354" alt="" />
                     </div>
                     <div className={styles.form__right}>
                         <h2 className={styles.form__title}>Add Movie</h2>
