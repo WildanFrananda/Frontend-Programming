e6d0dd822a81035a6700a55ded309668262e4cbf
WildanFrananda <wildanrailfanskrl@gmail.com>
2023-05-21 18:08:10 +0700

    Add popular movie using API and refactor Hero Component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,59 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useEffect, useState} from "react"
+import axios from "axios"
 import StyledHero from "./Hero.styled"
 import Button from "../ui/Button"
-import {useEffect, useState} from "react"
 
 function Hero() {
     // Create movie state
     const [movie, setMovie] = useState("")
 
-    async function fetchMovie() {
-        const url = "https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590"
-        // Fetching data from omdb API
-        const response = await fetch(url)
-        const data = await response.json()
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const genres = movie && movie.genres
+        .map((genre) => genre.name)
+        .join(", ")
+    const idTrailer = movie && movie.videos.results[0].key
 
-        // Update state movie
-        setMovie(data)
+    async function getTrendingMovie() {
+        const URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+        const response = await axios(URL)
+        return response.data.results[0]
     }
 
-    useEffect(function() {
-        fetchMovie()
-    }, [])
+    async function getDetailMovie() {
+        const trendingMovie = await getTrendingMovie()
+        const id = trendingMovie.id
+        const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
+        const response = await axios(URL)
+        setMovie(response.data)
+    }
 
-    console.log(movie)
+
+    useEffect(function() {
+        getDetailMovie()
+    }, [])
 
     return (
         <StyledHero>
             <section>
                 <div className="hero__left">
-                    <h2 variant="primary">{movie.Title}</h2>
-                    <h3 variant="secondary">{movie.Genre}</h3>
-                    <p>{movie.Plot}</p>
-                    <Button variant="primary" size="md">Watch Now</Button>
+                    <h2 variant="primary">{movie.title}</h2>
+                    <h3 variant="secondary">{genres}</h3>
+                    <p>{movie.overview}</p>
+                    <Button 
+                        variant="primary" 
+                        size="lg"
+                        as="a"
+                        href={`https://www.youtube.com/watch?v=${idTrailer}`}
+                        target="_blank">
+                            Watch Trailer
+                    </Button>
                 </div>
                 <div>
-                    <img src={movie.Poster} alt={movie.Title} />
+                    <img 
+                        src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                        alt="placeholder"
+                    />
                 </div>
             </section>
         </StyledHero>
--- src/pages/movie/Popular.jsx
+++ src/pages/movie/Popular.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useEffect, useState} from "react"
+import axios from "axios"
+import Movies from "../../components/Movies/Movies"
+import Hero from "../../components/Hero/Hero"
+
 function PopularMovie() {
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+
+    const [movies, setMovies] = useState([])
+
+    async function getPopularMovies() {
+        // Fetch fata from API
+        const response = await axios(URL)
+        setMovies(response.data.results)
+    }
+
+    useEffect(function() {
+        getPopularMovies()
+    }, [])
+
+    console.log(movies)
+
     return (
         <>
-            <h2>Popular Movie</h2>
+            <Hero />
+            <Movies movies={movies} />
         </>
     )
 }
